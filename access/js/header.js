6ff61c547328ac93316a6675b155c18f38d010ff
PHANHOP-PC\admin <phanhop24022001@gmail.com>
2021-09-22 08:45:37 +0700

    commit

---
+++ access/js/header.js
@@ -0,0 +1,54 @@
+function clickHidden(id){
+    var dropdown = document.getElementById(id);
+    dropdown.classList.toggle("show");
+    window.onclick = function(event){
+        if(!event.target.matches(".btn")){
+            if(dropdown.classList.contains("show")){
+                dropdown.classList.remove("show");
+            }
+        }
+    }
+}
+var move = null;
+function displayLogin(id){
+    var element = document.getElementById(id);
+    var posX = 0;
+    var posY = 0;
+    clearInterval(move);
+    move = setInterval(frame,1);
+    function frame(){
+        if(posX == 520){
+            clearInterval(move);
+        }
+        else{
+            posX+=52;
+            posY+=2;
+            element.style.top = posY + "px";
+            element.style.right = posX + "px";
+        }
+    }
+}
+// section slogan
+const sloganElement = document.querySelector(".section-slogan-desc");
+const sloganText = sloganElement.textContent;
+const sloganTextSplit = sloganText.split("");
+sloganElement.textContent = "";
+for(let i=0;i<sloganTextSplit.length;i++){
+    sloganElement.innerHTML += "<span>" + sloganTextSplit[i] + "</span>"
+}
+let char = 0;
+let timer = setInterval(onTick,50);
+function onTick(){
+    const span = sloganElement.querySelectorAll("span")[char];
+    span.classList.add("fadeSlogan");
+    char++;
+    if(char === sloganTextSplit.length){
+        complete();
+        return;
+    }
+}
+function complete(){
+    clearInterval(timer);
+    timer = null;
+}
+
